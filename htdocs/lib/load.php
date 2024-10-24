fe3e12dd2b48261a14d309644670a01542534ff1
vishnukumar321 <vishnu05357874@gmail.com>
2024-10-24 22:47:09 +0530

    project architecture upgraded

--- htdocs/lib/load.php
+++ htdocs/lib/load.php
@@ -3,10 +3,10 @@ include_once "lib/include/__database.php";
 include_once "lib/include/__user.php";
 session_start();
 function get_file($name){
-    include $_SERVER['DOCUMENT_ROOT']."/app1/app/lib/template/$name.php";
+    include $_SERVER['DOCUMENT_ROOT']."/app1/app/htdocs/lib/template/$name.php";
 }
 global $file;
-$file=file_get_contents($_SERVER['DOCUMENT_ROOT']."/app1/app/app3config.json");
+$file=file_get_contents($_SERVER['DOCUMENT_ROOT']."/app1/app/project/app3config.json");
 function get_conf($name){
     global $file;
     $arr=json_decode($file,true);
